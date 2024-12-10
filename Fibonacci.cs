bb7a4eb6900c5c5ce1394f21e370f42879ffc151
Nikita-Patil <37011059+Nikita-Patil@users.noreply.github.com>
2024-12-10 23:43:15 +0530

    Add files via upload

---
+++ Fibonacci.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDTestProject
+{
+	public class Fibonacci
+	{
+		public static List<int> GenerateFibonacci(int n)
+		{
+
+			List<int> fibonacciSeries = new List<int>();
+
+			if (n <= 0)
+				return fibonacciSeries;
+
+			fibonacciSeries.Add(0);
+			if (n > 1)
+			{
+				fibonacciSeries.Add(1);
+			}
+
+			for (int i = 2; i < n; i++)
+			{
+				int nextNumber = fibonacciSeries[i - 1] + fibonacciSeries[i - 2];
+				fibonacciSeries.Add(nextNumber);
+			}
+
+			return fibonacciSeries;
+		}
+	}
+}
